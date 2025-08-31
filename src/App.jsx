88823abbdfbd18e6e2e1e7a998baf4539eb43abf
Sujitha Neelam <163429668+sujitha769@users.noreply.github.com>
2025-08-31 11:17:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home.jsx'
+import Home from "./pages/Home.jsx";
+
 import Registerpage from './pages/Registerpage.jsx'
 
 const App = () => {
